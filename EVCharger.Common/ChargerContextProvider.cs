d8e5fec4f5e88e17211f9d2cfc3d44cd1a0f3411
Andrew Odintsov <a1covker@gmail.com>
2021-08-18 12:40:39 +0100

    Add charger context provider

---
+++ EVCharger.Common/ChargerContextProvider.cs
@@ -0,0 +1,14 @@
+﻿using EVCharger.Authentication.Interfaces;
+using EVCharger.Common.Protobuf.DB;
+
+namespace EVCharger.Common.Protobuf
+{
+   /// <summary>
+   /// This wrapper class allows us to instantiate DB context
+   /// Makes the code using it more unit-testable
+   /// </summary>
+   public class ChargerContextProvider : IChargerContextProvider
+   {
+      public ChargerContext Context => new();
+   }
+}
---
+++ EVCharger.Common/Interfaces/IChargerContextProvider.cs
@@ -0,0 +1,9 @@
+﻿using EVCharger.Common.Protobuf.DB;
+
+namespace EVCharger.Authentication.Interfaces
+{
+   public interface IChargerContextProvider
+   {
+      ChargerContext Context { get; }
+   }
+}
